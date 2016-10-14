4541984d7dcdcfdeee130152c2ac855e2a76dc02
Nan <latree88@gmail.com>
2016-10-14 14:42:15 -0700

    finish required problems for nine chapter problems!!!!!!!!!! yay!!!!!!!!!!!!!

--- FindTheMissingNumberii.java
+++ FindTheMissingNumberii.java
@@ -1,66 +1,52 @@
 public class FindTheMissingNumberii{
 	public static void main(String args[]){
-		String temp = "19201234567891011121314151618";
+		String temp = "121314151618192012345689101171722232524";
 
-		int rslt = new FindTheMissingNumberii().findMissing2(20, temp);
+		int rslt = new FindTheMissingNumberii().findMissing2(25, temp);
 
 		System.out.println(rslt);
 
 	}
 
-	public int findMissing2(int n, String str){
+    public boolean flag = false;
+    public int ans = 0;
+    public int findMissing2(int n, String str) {
+        boolean[] happen = new boolean[n + 1];
+        dfs(0, n, str, happen);
+        return ans;
+    }
+    
+    public void dfs(int i, int n, String s, boolean[] happen) {
+        if (i >= s.length() || flag) {
+        	if (!flag)
+            for (int k = 1; k <= n; k++) {
+                if (!happen[k]) {
+                    ans = k;
+                }
+            }
+        	flag = true;
+            return;
+        }
+        int sum = s.charAt(i) - '0';
+        int j = i;
+        if (sum == 0) {
+            return;
+        }
+        while (sum <= n) {
+            if (!happen[sum]) {
+                happen[sum] = true;
+                dfs(j+1, n, s, happen);
+                happen[sum] = false;
+            }
+            j++;
+            if (j >= s.length()) {
+                break;
+            }
+            sum = sum * 10 + (s.charAt(j) - '0');
+        }
+    }
 
-		int[] count = new int[10];
-		
-		// int sum = 0;
-		for(int i = 1; i < n + 1; i++){
 
-			int firstDigit = i % 10;
-			int secondDigit = i / 10;
-			// sum += firstDigit + secondDigit;
-
-			if(secondDigit == 0){
-				++count[firstDigit]; 
-			}
-			else{
-				++count[firstDigit];
-				++count[secondDigit];
-			}
-
-		}
-
-		for(int i = 0; i < str.length(); i++){
-			int val = Character.getNumericValue(str.charAt(i));
-			// sum -= val;
-			--count[val];
-		}
-
-		int[] twoDigits = findNumber(count);
-
-		// sum is the sum of first digit and second digit
-		if(twoDigits[0]*10 + twoDigits[1] <= n){
-			return twoDigits[0]*10 + twoDigits[1];
-		}
-		else{
-			return twoDigits[1]*10 + twoDigits[0];
-		}
-	}
-
-	public int[] findNumber(int[] source){
-		int[] target = new int[2];
-		int index = 0;
-		for(int i = 0; i < source.length; i++){
-			if(source[i] != 0){
-				target[index] = i;
-				++index;
-				if(index > 1){
-					return target;
-				}
-			}
-		}
-
-		return target;
-	}
 }
 
 
